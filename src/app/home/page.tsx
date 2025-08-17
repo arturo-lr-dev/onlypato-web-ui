9d675c351b9d06a6939eec1c050c7ec393586bfc
Arturo Legaspi Rodrigo <artuleg@gmail.com>
2025-08-17 12:04:42 +0200

    feat(analytics): add conversion tracking for home page
    Implement gtag conversion tracking to measure marketing campaign effectiveness

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,28 @@
 'use client';
 
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { DuckHeroSection } from '../components/DuckHeroSection';
 import PromoHeader from '../components/PromoHeader';
 import { DuckMenu } from '../components/DuckMenu';
 
 const Hero = () => {
 
-  useMemo(() => {
-    window.gtag('config', 'G5nLCJahifoYEN_sx9UC', {
-      page_title: 'Home',
-      page_location: '/'
-    });
+  function gtag_report_conversion() {
+    var callback = function () {
+      
+    };
+
+    if (window.gtag) {
+      window.gtag('event', 'conversion', {
+        'send_to': 'AW-716306015/G5nLCJahifoYEN_sx9UC',
+        'event_callback': callback
+      });
+    }
+    return false;
+  }
+
+  useEffect(() => {
+    gtag_report_conversion();
   }, []);
 
   return (
